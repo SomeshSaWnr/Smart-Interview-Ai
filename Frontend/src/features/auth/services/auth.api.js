6136675d71e3ab3e814dfcd8f16796224fde2dec
SomeshSaWn <sawnersomesh79@gmail.com>
2026-05-23 13:02:59 +0530

    prod backend connection

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://smart-interview-ai-ur4a.onrender.com",
     withCredentials: true
 })
 
